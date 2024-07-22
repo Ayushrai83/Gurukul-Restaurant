1d9d21835a0d64bef5e8ac2e4fe76fa19e3418ef
Ayushrai83 <gurukul4ayush@gmail.com>
2024-07-22 19:56:33 +0530

    Added Section Contact

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,6 +6,7 @@ import Section3 from "./Section3";
 import Section4 from "./Section4";
 import Section5 from "./Section5";
 import Section6 from "./Section6";
+import Section7 from "./Section7";
 
 const Home = () => {
   return (
@@ -28,6 +29,9 @@ const Home = () => {
 
         {/* Home Section Blog */}
         <Section6 />
+
+        {/* Home Section Contact */}
+        <Section7 />
        </Layout>
     </>
   );
