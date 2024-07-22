92cc240cd17b90ba4b7080ccac3d523a6621a7db
Ayushrai83 <gurukul4ayush@gmail.com>
2024-07-22 13:26:02 +0530

    Frontend template

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../../components/Layouts/Layout";
+import Section1 from "./Section1";
+
+const Home = () => {
+  return (
+    <>
+      <Layout>
+        {/* Home Section Hero Banner */}
+        <Section1 />
+      </Layout>
+    </>
+  );
+};
+
+export default Home;
